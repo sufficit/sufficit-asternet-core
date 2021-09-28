9d927425f2a4ed8b0882083e4285f5766e2f10c1
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2021-09-28 19:05:11 -0300

    interfaces and asterisk versions

---
+++ src/Manager/IManagerConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsterNET.Manager
+{
+    public interface IManagerConnection
+    {
+        int ReconnectIntervalMax { get; set; }
+        bool FireAllEvents { get; set; }
+        int PingInterval { get; set; }
+        string Hostname { get; set; }
+        int Port { get; set; }
+        string Username { get; set; }
+        string Password { get; set; }
+        int SleepTime { get; set; }
+        int DefaultEventTimeout { get; set; }
+        int ReconnectIntervalFast { get; set; }
+        bool KeepAliveAfterAuthenticationFailure { get; set; }
+        bool KeepAlive { get; set; }
+        Encoding SocketEncoding { get; set; }
+        int SocketReceiveBufferSize { get; set; }
+        string Version { get; }
+        int DefaultResponseTimeout { get; set; }
+        int ReconnectRetryMax { get; set; }
+        AsteriskVersion AsteriskVersion { get; }
+        int ReconnectRetryFast { get; set; }
+    }
+}
