ef641ffea3efc0f806b5da9f86144b51b2530a11
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2021-09-28 23:00:01 -0300

    imanager

--- src/Manager/IManagerConnection.cs
+++ src/Manager/IManagerConnection.cs
@@ -6,24 +6,11 @@ namespace AsterNET.Manager
 {
     public interface IManagerConnection
     {
-        int ReconnectIntervalMax { get; set; }
-        bool FireAllEvents { get; set; }
-        int PingInterval { get; set; }
         string Hostname { get; set; }
-        int Port { get; set; }
-        string Username { get; set; }
-        string Password { get; set; }
-        int SleepTime { get; set; }
-        int DefaultEventTimeout { get; set; }
-        int ReconnectIntervalFast { get; set; }
-        bool KeepAliveAfterAuthenticationFailure { get; set; }
         bool KeepAlive { get; set; }
-        Encoding SocketEncoding { get; set; }
         int SocketReceiveBufferSize { get; set; }
         string Version { get; }
-        int DefaultResponseTimeout { get; set; }
-        int ReconnectRetryMax { get; set; }
         AsteriskVersion AsteriskVersion { get; }
-        int ReconnectRetryFast { get; set; }
+        bool IsConnected();
     }
 }
